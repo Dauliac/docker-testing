a2c8473d26f4e38d8987a3d2b59f968f2c287383
Rraph <raphael.beghin@ynov.com>
2018-05-26 16:39:06 +0200

    ajout de grafana

--- compose.sh
+++ compose.sh
@@ -3,4 +3,5 @@ docker-compose --file "src/docker-compose.yml" up --build --force-recreate -d
 echo 'Go on :
     http://localhost:9000                  => portainer
     http://localhost:8080                  => owncloud
-    http://localhost:32400/web => plex '
+    http://localhost:32400/web             => plex
+    http://localhost:3000                  => vérifiez létat de vos services avec grafana !'
