70de7a210acdc186dce9ec77379ebe7ddc6d2017
dauliac <juliendauliac@gmail.com>
2018-06-01 01:52:06 +0200

    final
    Signed-off-by: dauliac <juliendauliac@gmail.com>

--- compose.sh
+++ compose.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 docker-compose --file "src/docker-compose.yml" up --build --force-recreate -d
-echo 'Go on :
-    http://0.0.0.0:9000                  => portainer
-    http://0.0.0.0:8080                  => owncloud
-    http://0.0.0.0:32400/web             => plex
-    http://0.0.0.0:8005/docker/          => cadvisor
-    http://0.0.0.0:3000                  => grafana !'
+echo 'Services :
+    - portainer  =>  http://0.0.0.0:9007
+    - plex       =>  http://0.0.0.0:32400
+    - owncloud   =>  http://0.0.0.0:9005
+    - mariadb    =>  mysql://0.0.0.0:3306
+    - cadvisor   =>  http://0.0.0.0:9004
+    - prometheus =>  http://0.0.0.0:9090
+    - grafana    =>  http://0.0.0.0:3000
+    - mcmyadmin  =>  http://0.0.0.0:9001
+    - minecraft  =>  0.0.0.0:2556'
